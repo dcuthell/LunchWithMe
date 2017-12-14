9d9425ac428bb0144195173878e76598b2cfb328
Kate Byars <katebyars5@gmail.com>
2017-12-13 17:23:53 -0800

    add working slider.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,7 +4,7 @@ import { AppRegistry, StyleSheet, View, Text } from "react-native";
 
 export default class SliderOne extends React.Component {
   state = {
-    value: 0.2
+    value: 0
   };
 
   render() {
@@ -13,9 +13,12 @@ export default class SliderOne extends React.Component {
         <Slider
           value={this.state.value}
           onValueChange={value => this.setState({ value })}
+          minimumTrackTintColor = 'gray'
+          minimumValue = { .5 }
+          maximumValue = { 30 }
         />
-        <Text>
-          Value: {this.state.value}
+      <Text style={styles.text}>
+          Years of Experience: {this.state.value}
         </Text>
       </View>
     );
@@ -25,9 +28,13 @@ export default class SliderOne extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 8,
     marginLeft: 10,
     marginRight: 10,
     alignItems: "stretch",
     justifyContent: "center"
+  },
+  text: {
+    paddingBottom: 8,
   }
 });
