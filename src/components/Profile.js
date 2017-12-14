239a405eaffd262875e5a6d80144fdfabc006deb
Kate Byars <katebyars5@gmail.com>
2017-12-13 16:32:28 -0800

    add switches to the profile form.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native'
+import { StyleSheet, Text, View, Image, TextInput, Switch} from 'react-native'
 import { FormLabel, FormInput, Button } from 'react-native-elements'
 
 
@@ -10,15 +10,68 @@ export default class Profile extends React.Component {
       <View style={styles.container}>
           <FormLabel labelStyle={styles.formLabel}>A bit about me</FormLabel>
         <FormInput inputStyle={styles.formInput}/>
+        <Text style={styles.text}>My Skills</Text>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>React-Native</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="Save my details" border={false} name="save_details" />
+            </View>
+        </View>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>Python</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="python" border={false} name="python" />
+            </View>
+        </View>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>Swift</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="python" border={false} name="python" />
+            </View>
+        </View>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>PHP</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="python" border={false} name="python" />
+            </View>
+        </View>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>.NET</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="python" border={false} name="python" />
+            </View>
+        </View>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>JavaScript</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="python" border={false} name="python" />
+            </View>
+        </View>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>Perl</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="python" border={false} name="python" />
+            </View>
+        </View>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>Android</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="python" border={false} name="python" />
+            </View>
+        </View>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>CSS</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="python" border={false} name="python" />
+            </View>
+        </View>
+        <View style={styles.row}>
+          <FormLabel labelStyle={styles.formLabel}>HTML</FormLabel>
+            <View style={styles.switch} >
+              <Switch label="python" border={false} name="python" />
+            </View>
+        </View>
 
-
-        {/* <Button
-          raised
-          icon={{name: 'home', size: 32}}
-          buttonStyle={{backgroundColor: 'red', borderRadius: 10}}
-          textStyle={{textAlign: 'center'}}
-          title={`Welcome to\nReact Native Elements`}
-        /> */}
       </View>
     )
   }
@@ -35,8 +88,30 @@ const styles = StyleSheet.create({
   formLabel: {
     color: '#42b3f4',
     fontFamily: 'Avenir',
-    fontSize: 14,
+    fontSize: 15,
     textAlign: 'left'
+  },
+  switch: {
+    fontSize: 15,
+    fontFamily: 'Avenir',
+    color: '#42b3f4',
+    flex: 1,
+    flexDirection: 'row',
+    paddingTop: 7,
+    paddingRight: 20,
+    justifyContent: 'flex-end'
+  },
+  text: {
+    // paddingTop: 15,
+    fontSize: 15,
+    fontFamily: 'Avenir',
+    color: 'gray',
+    flexDirection: 'row',
+    textAlign: 'left',
+    paddingLeft: 19,
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
   }
-
 })
