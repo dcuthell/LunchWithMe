4a9dc25eab3713768f4636115fe4495ac83d0f38
dcuthell <dcc1447@gmail.com>
2017-12-18 15:13:20 -0800

    make post work for login call, returns user id

--- src/ajax.js
+++ src/ajax.js
@@ -3,32 +3,29 @@ const apiHost = 'http://lunchwith-api.eyecue.io';
 export default async function getLoginResponse() {
   console.log(apiHost+ '/login');
 
-  // const headers = new Header({
-  //
-  // })
-  //
-  // const request = new Request(apiHost+ '/login', {
-  //   method: 'POST',
-  //   headers: {
-  //     "Accept": "application/json",
-  //     "Content-Type": "application/vnd.api+json",
-  //   },
-  //   body: JSON.stringify({
-  //     "data": {
-  //       "attributes": {
-  //         "email": "bob@jones.com",
-  //         "password": "bobjones"
-  //       }
-  //     }
-  //   })
-  // });
 
-  const request = new Request('http://lunchwith-api.eyecue.io/session');
+
+  const request = new Request(apiHost+ '/login', {
+    method: 'POST',
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/vnd.api+json",
+    },
+    body: JSON.stringify({
+      "data": {
+        "attributes": {
+          "email": "bob@jones.com",
+          "password": "bobjones"
+        }
+      }
+    })
+  });
   try {
     console.log(request);
     const response = await fetch(request);
-    const responseJson = response.json();
-    console.log(responseJson);
+    const responseJson = await response.json();
+    const greeting = responseJson.included[0].id;
+    console.log(greeting);
   } catch (error) {
     console.log(error);
   }
