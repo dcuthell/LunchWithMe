d579a2567773598d453e6eed12eea0daeb91aac7
dcuthell <dcc1447@gmail.com>
2017-12-20 22:15:56 -0800

    add new app navigation and store files to follow template format

---
+++ src/navigator.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { StackNavigator, addNavigationHelpers } from 'react-navigation';
+
+import Login from 'components/containers/LoginScreen';
+import routesNames from 'lib/constants/routes';
+
+//TODO: Import and fix routes
+const routes = {
+  [routesNames.Login]: {
+    screen: Login,
+    navigationOptions: {
+      header: null,
+    },
+  },
+};
+
+//Feed in routes
+const AppNavigator = StackNavigator(routes);
+
+//Set up for props
+const AppWithoutNavigationState = props => (
+  <AppNavigator
+    navigation={addNavigationHelpers({
+      dispatch: props.dispatch,
+      state: props.nav,
+    })}
+  />
+);
+
+function mapNavToProps(store) {
+  return store;
+}
+
+//Connect
+const AppWithNavigationState = connect(mapNavToProps)(AppWithoutNavigationState);
+
+//TODO: Set initial state
+//const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('Login'));
+
+const navReducer = (state, action) => {
+  const newState = AppNavigator.router.getStateForAction(action, state);
+  return newState || state;
+};
+
+export default { AppWithNavigationState, navReducer };
